8d29bba47ff8baeb6dc162d65481a378f987118a
satoshi1979 <satoshi19791212@gmail.com>
2025-01-14 22:02:13 +0900

    一旦完成と思われる

---
+++ app/Http/Controllers/GreetingController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Egulias\EmailValidator\Parser\Comment;
+use Illuminate\Http\Request;
+
+class GreetingController extends Controller
+{
+    public function greeting($msg)
+    {
+        switch ($msg) {
+            case 'morning':
+                $page_title = '朝のあいさつ';
+                $greeting = 'おはようございます';
+                break;
+            case 'afternoon':
+                $page_title = '昼のあいさつ';
+                $greeting = 'こんにちわ';
+                break;
+            case 'evening':
+                $page_title = '夕方のあいさつ';
+                $greeting = 'こんばんは';
+                break;
+            case 'night':
+                $page_title = '夜のあいさつ';
+                $greeting = 'おやすみ';
+                break;
+            case 'random':
+                $page_title = 'ランダムなメッセージ';
+                $set_of_greetings = ['おはよう', 'こんにちは', 'こんばんは', 'おやすみ'];
+                $greeting = $set_of_greetings[rand(0, 3)];
+                break;
+            default:
+                $page_title = 'morning,afternoon,evening,night or random';
+                $greeting = 'ここにあいさつが入ります。';
+        }
+
+
+        return view(
+            'comments/greeting',
+            ['page_title' => $page_title, 'greeting' => $greeting]
+        );
+    }
+
+    public function freeword($msg)
+    {
+        return view(
+            'comments/freeword',
+            ['msg' => $msg]
+        );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\GreetingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/comments/{msg}', [GreetingController::class, 'greeting']);
+Route::get('/comments/freeword/{msg}', [GreetingController::class, 'freeword']);
